bfdfacdaf96d7b7aa15a7331fb57877409b275f5
hongltgch230156-prog <hongltgch230156@fpt.edu.vn>
2025-11-27 18:03:37 +0700

    fix app.js

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-//import express framwork
-const express = require('express')
-const app = express() //declare express
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-//import mongoose
-const mongoose = require('mongoose')
+const DATABASE_URI = "mongodb+srv://hongltgch230156:TYX4nmvIWGVL99tQ@mydbcluster.tjwmhci.mongodb.net/mood_journal";
 
-//declare db connection string URI with db name
+mongoose.connect(DATABASE_URI)
+  .then(() => console.log('Connect to DB succeed!'))
+  .catch((err) => console.log('Connect to DB failed! ' + err));
 
-//const DATABASE_URI = "mongodb://localhost:27017/mood_journal"
-//mongoose.connect(DATABASE_URI) //connect to db
-const db = 'mongodb+srv://hongltgch230156:<TYX4nmvIWGVL99tQ>@mydbcluster.tjwmhci.mongodb.net/mood_journal'
-.then(() => console.log('Connect to DB succeed!'))
-.catch((err) => console.log('Connect to DB failed!' + err))
+app.use(express.json());
 
-app.use(express.json())  //parser(input)
+app.use(cors()); 
 
-//import cors
-const cors = require('cors') 
-const corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
-
-//route
+// Route
 const moodRouter = require('./api/routes/moodRoute');
 moodRouter(app);
 const userRouter = require('./api/routes/userRoute');
 userRouter(app);
 
-//start server
-// const port = 5000;
-// app.listen(port, () => {
-//     console.log("Server is running at port " + port);
-// })
-
-app.listen(process.env.PORT || 3001)
-
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log("Server is running at port " + port);
+});
